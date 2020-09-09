9dd75e2bfc53d8f9edb1b190a30a3e93e43c9d00
maualvm@gmail.com <maualvm@gmail.com>
2020-09-08 19:32:12 -0500

    Initial commit

---
+++ app/src/main/java/mx/tec/example/example3/ChildActivity.java
@@ -0,0 +1,27 @@
+package mx.tec.example.example3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    Intent i;
+    TextView myTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        myTextView = findViewById(R.id.textView);
+
+        i = getIntent();
+        String auxString;
+
+        auxString = i.getStringExtra("myParam");
+
+        myTextView.setText(auxString);
+    }
+}
